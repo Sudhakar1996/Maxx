abb28f20a56a4052b1678f8e1fa021e57b98a3dc
Sudhakar1996 <sudhawoods@gmail.com>
2017-02-15 08:51:50 +0530

    Second commit

--- NewJFrame.java
+++ NewJFrame.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+//new commit
 public class NewJFrame extends javax.swing.JFrame {
 
     /**
